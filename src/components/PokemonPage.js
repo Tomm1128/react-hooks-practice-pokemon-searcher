2c29ab733bae12ffa75edfb576ee1be1dbae32cc
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-24 15:57:45 -0400

    Display pokemon cards

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,21 @@
-import React from "react";
-import PokemonCollection from "./PokemonCollection";
-import PokemonForm from "./PokemonForm";
-import Search from "./Search";
-import { Container } from "semantic-ui-react";
+import React, { useEffect, useState } from "react"
+import PokemonCollection from "./PokemonCollection"
+import PokemonForm from "./PokemonForm"
+import Search from "./Search"
+import { Container } from "semantic-ui-react"
+import { getPokemon } from "../services/fetchers"
 
 function PokemonPage() {
+  const [pokemonList, setPokemonList] = useState(null)
+
+  useEffect(() => {
+    getPokemon().then((pokemons) => setPokemonList(pokemons))
+  }, [])
+
+  if (!pokemonList) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
@@ -13,9 +24,9 @@ function PokemonPage() {
       <br />
       <Search />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemonList={pokemonList} />
     </Container>
-  );
+  )
 }
 
-export default PokemonPage;
+export default PokemonPage
