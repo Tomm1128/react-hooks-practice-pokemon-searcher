f27c46d4ee7bde6057ad2e4e1909f8ed9d5aa2c7
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-25 09:11:35 -0400

    Change sprite direction when clicked

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import { Card } from "semantic-ui-react"
 
 function PokemonCard({ pokemon: { id, name, hp, sprites } }) {
+  const [sprite, setSprite] = useState(true)
+
   return (
     <Card>
       <div>
-        <div className="image">
-          <img src={sprites.front} alt="oh no!" />
+        <div className="image" onClick={() => setSprite(!sprite)}>
+          <img src={sprite ? sprites.front : sprites.back} alt="oh no!" />
         </div>
         <div className="content">
           <div className="header">{name}</div>
